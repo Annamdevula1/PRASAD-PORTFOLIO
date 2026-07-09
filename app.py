8d8e143250b8f9ca7599a02c57e0358064fc7c86
ANNAMDEVULA DURGA PRASAD <durgaprasadannamdevula41@gmail.com>
2026-07-09 13:06:57 +0530

    Correct skills column syntax in app.py
    Fix indentation and syntax error in skills column.

--- app.py
+++ app.py
@@ -65,10 +65,10 @@ Technical Interests:
 st.header("💻 Skills")
 
 skills_col1, skills_col2 = st.columns(2)
-
-    with skills_col 1:
+with skills_col 1:
     st.write("✅ Python")
     st.write("✅ Machine Learning")
+with skills_col2:
     st.write("✅ Azure")
     st.write("✅ Artificial Intelligience")
 
